e14794154ec05013650f7a5d1bab28d1a81b0d22
Maximilian Persson <Maximilian.persson2@elev.ga.ntig.se>
2024-01-26 10:14:16 +0100

    first commit

---
+++ RestingBFace/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+using RestSharp;
+using System.IO;
+using System.Runtime.InteropServices;
+
+RestClient client = new("https://swapi.py4e.com/api/");
+Console.WriteLine("Which Star Wars Character");
+
+string starWarsNumber = Console.ReadLine();
+RestRequest request = new($"people/{starWarsNumber}");
+
+RestResponse response = client.GetAsync(request).Result;
+
+if (response.StatusCode == System.Net.HttpStatusCode.OK)
+{
+    StarWars SW = JsonSerializer.Deserialize<StarWars>(response.Content);
+    Console.WriteLine(SW.name);
+}
+else
+{
+    Console.WriteLine("ssss");
+}
+Console.WriteLine(response.Content);
+File.WriteAllText("StarWars.json", response.Content);
+Console.ReadLine();
---
+++ RestingBFace/StarWars.cs
@@ -0,0 +1,9 @@
+using System.Text.Json.Serialization;
+public class StarWars
+{
+public int id {get; set;}
+
+public int starWarsNumber {get; set;}
+
+public string name {get; set;}
+}
